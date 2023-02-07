411d952679724b6c1c2ec10d6e6bcafcd15186bb
bhokaremoin <75439850+bhokaremoin@users.noreply.github.com>
2023-02-07 22:56:21 +0530

    fixed error of small letter in file name

---
+++ front-end/src/Components/Section/SummaryBody.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useGlobalContext } from "../../contextAPI";
+import InputControl from "../InputControl";
+import styles from "../Styles/Editor.module.css";
+const SummaryBody = () => {
+  const { values, setValues } = useGlobalContext();
+  return (
+    <div className={styles.detail}>
+      <InputControl
+        label="Summary"
+        value={values.summary}
+        placeholder="Enter your objective/summary"
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, summary: event.target.value }))
+        }
+      />
+      <div></div>
+    </div>
+  );
+};
+
+export default SummaryBody;
